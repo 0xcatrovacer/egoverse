6fd107f6f7c17f78802b4ba2c84de1b49ae8e926
Ace <51465755+AceVikings@users.noreply.github.com>
2023-09-16 00:09:00 +0530

    init

---
+++ egoverse-nft-staking/programs/egoverse-nft-staking/src/instructions/mod.rs
@@ -0,0 +1,11 @@
+pub use close_staking::*;
+pub use init_staking::*;
+pub use stake::*;
+pub use unstake::*;
+pub use withdraw_reward::*;
+
+pub mod close_staking;
+pub mod init_staking;
+pub mod stake;
+pub mod unstake;
+pub mod withdraw_reward;
---
+++ egoverse-nft-staking/programs/egoverse-nft-staking/src/state/mod.rs
@@ -0,0 +1,5 @@
+mod nft_record;
+mod stake_details;
+
+pub use nft_record::*;
+pub use stake_details::*;
